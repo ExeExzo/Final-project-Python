2b5809a24dc10b85a943f4711101f36a57a13175
slonoboy <52863393+slonoboy@users.noreply.github.com>
2021-11-24 00:01:51 +0600

    Add files via upload

---
+++ test/test.py
@@ -0,0 +1,7 @@
+from summarizer import Summarizer
+
+sum = Summarizer()
+article_summary = sum.summarize('https://hackernoon.com/will-the-game-stop-with-gamestop-or-is-this-just-the-beginning-2j1x32aa')
+print(article_summary)
+
+
